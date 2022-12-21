93813274642af63d76e4654b3de6720aba342c06
Vikas Gupta <vg2292097@gmail.com>
2022-12-21 22:58:25 +0530

    Add script api for read file and execute script

--- app.js
+++ app.js
@@ -11,8 +11,26 @@ app.get('/',(req,res)=>{
 app.get('/execute',async (req,res)=>{
     try {
         const {command} = req.body;
-        let response = await executeShellScriptCommand(command);
+        let response = await executeCommand(command);
 
+        res.status(response.status).send({
+            status:response.status,
+            message:response.message,
+            response:response
+        })
+    } catch (error) {
+        res.status(500).send({
+            status:500,
+            message:error.message,
+            response:error
+        })
+    }
+})
+app.get('/executeScript',async (req,res)=>{
+    try {
+        const {command} = req.body;
+        await executeShellScriptCommand(command);
+        let response = await readFile(command);
         res.status(response.status).send({
             status:response.status,
             message:response.message,
@@ -29,7 +47,7 @@ app.get('/execute',async (req,res)=>{
 app.get('**',(req,res)=>{
     res.send("Page Not Found!")
 })
-const executeShellScriptCommand = (cmd)=>{
+const executeCommand = (cmd)=>{
     return new Promise(function(resolve, reject) {
         exec('ls -lh', (error, stdout, stderr) => {
             if (error) {
@@ -43,5 +61,30 @@ const executeShellScriptCommand = (cmd)=>{
 
           });
         });
+}
+const executeShellScriptCommand = (cmd)=>{
+    return new Promise(function(resolve, reject) {
+        exec('bash sciptForRemoveNodeModule.sh > logsfile.txt', (error, stdout, stderr) => {
+            if (error) {
+              console.error(`error: ${error.message}`);
+            }
+            resolve({
+                response: stdout ? stdout : stderr,
+                message: stdout ? "Success" : `stderr ${stderr}`,
+                status: stdout ? 200 : 400
+            })
+
+          });
+        });
+}
+const readFile = (cmd)=>{
+    // Include fs module
+    var fs = require('fs');
+    return new Promise((resolve, reject) => {
+        // Use fs.readFile() method to read the file
+        fs.readFile('logsfile.txt', 'utf8', (err, data) => {
+            resolve({status:data ? 200 : 400, message: data ? "Success" : err.message , response: data ? data: err})
+        })
+    })
 }
 app.listen(PORT,()=>console.log(`App is listean on http://localhost:${PORT}/`))
