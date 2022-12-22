143c8dc6e91482842edd6b1a2516c0605224edf0
Vikas Gupta <vg2292097@gmail.com>
2022-12-22 21:12:46 +0530

    App.js code optimsize

--- app.js
+++ app.js
@@ -9,30 +9,11 @@ app.get('/',(req,res)=>{
     res.send("Welcome to Page!")
  })
 
-app.get('/execute',async (req,res)=>{
-    try {
-        const {command} = req.body;
-        let response = await executeCommand(command);
-
-        res.status(response.status).send({
-            status:response.status,
-            message:response.message,
-            response:response
-        })
-    } catch (error) {
-        res.status(500).send({
-            status:500,
-            message:error.message,
-            response:error
-        })
-    }
-})
 app.post('/executeScript',async (req,res)=>{
     try {
         let {command} = req.body;
-        command = command ? command : 'bash sciptForRemoveNodeModule.sh > logsfile.txt';
-        await executeShellScriptCommand(command);
-        let response = await readFile(command);
+        let response = await executeShellScriptCommand(command);
+        // let response = await readFile(command);
         res.status(response.status).send({
             status:response.status,
             message:response.message,
@@ -53,13 +34,9 @@ app.get('/removenodemodules',async (req,res)=>{
         if(!fs.existsSync(`${__dirname}/node_modules`) && fs.existsSync(`${__dirname}/package.json`)){
             cmd = `cd ${__dirname}/ && npm i`
         }
-       cmd = script ? "sh script.sh >> logsfiles.txt" : cmd;
+        cmd = !script ? cmd : "sh script.sh >> logsfiles.txt";
         let rmnodemodules = await executeShellScriptCommand(cmd);
-        if(rmnodemodules.message.indexOf("npm WARN") > -1 || rmnodemodules.status === 200 ){
-            res.send({rmnodemodules,status:200})
-        }else{
-            res.send({rmnodemodules,status:400,message:"Something went wrong!!!"})
-        }
+        res.send(rmnodemodules);
     } catch (error) {
         res.send({response:error,status:500,message:"Something went wrong!!!"})
     }
@@ -68,21 +45,6 @@ app.get('/removenodemodules',async (req,res)=>{
 app.get('**',(req,res)=>{
     res.send("Page Not Found!")
 })
-const executeCommand = (cmd)=>{
-    return new Promise(function(resolve, reject) {
-        exec('ls -lh', (error, stdout, stderr) => {
-            if (error) {
-              console.error(`error: ${error.message}`);
-            }
-            resolve({
-                response: stdout ? stdout : stderr,
-                message: stdout ? "Success" : `stderr ${stderr}`,
-                status: stdout ? 200 : 400
-            })
-
-          });
-        });
-}
 const executeShellScriptCommand = (cmd)=>{
     return new Promise(function(resolve, reject) {
         exec(cmd, (error, stdout, stderr) => {
